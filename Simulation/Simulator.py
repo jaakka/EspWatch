1b2b174b86f55baa752bad14bc5dc1f243330286
Jaakko Talvitie <jaska.talvitie@gmail.com>
2024-10-01 23:46:15 +0300

    second window added for simulator

--- Simulation/Simulator.py
+++ Simulation/Simulator.py
@@ -1,28 +1,54 @@
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout
 from PyQt5.QtGui import QPainter, QPen, QBrush, QFont
 from PyQt5.QtCore import Qt
 
 class MyWidget(QWidget):
-    def paintEvent(self, event):
-        self.resize(400, 400)
-        self.setStyleSheet("background-color: black;")
-        painter = QPainter(self)
-        
-        brush = QBrush(Qt.gray)  # Pallon täyttöväri
-        painter.setBrush(brush)
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
+    def initUI(self):
+        # Asetetaan pääikkuna ja sen koko
+        self.setWindowTitle('EspWatch simulator')
+        self.resize(400, 400)
+
+        # Näytetään toinen ikkuna heti sovelluksen käynnistyessä
+        self.openSecondWindow()
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+
+        # Piirretään pallo
+        brush = QBrush(Qt.gray)
+        painter.setBrush(brush)
         pen = QPen(Qt.gray, 5, Qt.SolidLine)
         painter.setPen(pen)
-        painter.drawEllipse(25, 25, 350, 350)  # x, y, leveys, korkeus
-        self.setWindowTitle('EspWatch simulator')
+        painter.drawEllipse(25, 25, 350, 350)
 
+        # Piirretään teksti
         pen = QPen(Qt.black, 5, Qt.SolidLine)
         painter.setPen(pen)
-
-        font = QFont('Arial', 12)  # Arial fontti, 30 pisteen koko
+        font = QFont('Arial', 12)
         painter.setFont(font)
-
         painter.drawText(140, 190, 'System starting...')
+
+    # Metodi toisen ikkunan avaamiseksi
+    def openSecondWindow(self):
+        self.secondWindow = SecondWindow()
+        self.secondWindow.show()
+
+class SecondWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # Asetetaan toisen ikkunan otsikko ja koko
+        self.setWindowTitle('Toinen ikkuna')
+        self.resize(300, 200)
+
+        # Asetetaan taustaväri toiseen ikkunaan
+        self.setStyleSheet("background-color: lightgray;")
 
 app = QApplication([])
 widget = MyWidget()
