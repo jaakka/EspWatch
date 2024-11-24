6863c78ebbb747f6a61a2e64c9813733317abf33
pate313 <143716706+pate313@users.noreply.github.com>
2024-11-24 02:35:20 +0200

    Declared gyroscope int pin, commented serial debug message
    Gyroscope interrupt pin is for possible future use
    Commented serial debug message as it's not needed anymore

--- Hardware/EspWatch/GyroscopeSensor.ino
+++ Hardware/EspWatch/GyroscopeSensor.ino
@@ -1,9 +1,12 @@
+constexpr int gyroIntPin = 23;
+
+
 void GyroscopeUpdate()
 {
   if(GYRO.gyroscopeAvailable()) 
   {
     GYRO.readGyroscope(gyro_x, gyro_y, gyro_z);
     GYRO.readAcceleration(gyro_ax,gyro_ay,gyro_az);
-    Serial.println(String(gyro_ax) + ", "+String(gyro_ay) + ", "+String(gyro_az));
+    //Serial.println(String(gyro_ax) + ", "+String(gyro_ay) + ", "+String(gyro_az));
   }
 }
