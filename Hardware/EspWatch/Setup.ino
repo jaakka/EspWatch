628df31348b34f936a700de5e52c1a55cab92b78
pate313 <143716706+pate313@users.noreply.github.com>
2024-11-24 02:42:00 +0200

    Added serial init, pcf8591 init and HallInit
    Added serial.begin
    
    Added delay to heartrate sensor connection
    
    Added pcf.begin to connect to PCF8591
    
    Added PCF8591_Init and HallInit to run in setup

--- Hardware/EspWatch/Setup.ino
+++ Hardware/EspWatch/Setup.ino
@@ -1,5 +1,8 @@
 void setup()
 {
+  Serial.begin(9600);
+  while (!Serial) {}
+
   int allowed_try_times = 100;
   while(!GYRO.begin() && allowed_try_times > 0) // Force gyroscope
   {
@@ -13,15 +16,29 @@ void setup()
   {
     allowed_try_times--;
     Serial.println("Failed to initialize HeartRate Sensor!");
+    delay(10);
   }
 
   if(HEARTRATE.setSamplingRate(kSamplingRate)) 
   { 
     Serial.println("HeartRate sensor initialized");
   }
+
+  allowed_try_times = 100;
+  while(!pcf.begin() && allowed_try_times > 0)
+  {
+    allowed_try_times--;
+    Serial.println("Failed to initialize PCF8591!");
+    delay(10);
+  }
+
+
+
   BtnsInit();
   LCD.begin();
   LcdStart();
+  PCF8591_Init();
+  HallInit();
 
   Serial.begin(9600);
 }
