da189ba146cd8b9ee7fa2c4a529cd771da78a088
Paavo Perämäki <pate313@gmail.com>
2024-12-25 01:43:34 +0200

    Working bpm and SpO2 functionality
    Added bpm and SpO2 functionality, using mostly modified example code from the MAX3010x sensor library for now

--- Hardware/EspWatchObjectOriented/main/heartrate.cpp
+++ Hardware/EspWatchObjectOriented/main/heartrate.cpp
@@ -4,7 +4,16 @@
 #include "heartratefilters.h"
 
 
-HEARTRATE::HEARTRATE() : obj(MAX30102()) {}
+HEARTRATE::HEARTRATE() 
+  : obj(MAX30102()),
+    // Filter Instances
+    low_pass_filter_red(HEARTRATE::kLowPassCutoff, kSamplingFrequency),
+    low_pass_filter_ir(HEARTRATE::kLowPassCutoff, kSamplingFrequency),
+    high_pass_filter(HEARTRATE::kHighPassCutoff, kSamplingFrequency),
+    differentiator(kSamplingFrequency),
+    averager_bpm(),
+    averager_r(),
+    averager_spo2() {}
 
 bool HEARTRATE::begin() {
     if (obj.begin()) 
@@ -22,44 +31,144 @@ bool HEARTRATE::begin() {
 }
 
 void HEARTRATE::enableSensor() {
+    // TODO: Implement enableSensor() 
     sensorEnabled = true;
 }
 
 void HEARTRATE::disableSensor() {
+    // TODO: Implement disableSensor()
     sensorEnabled = false;
 }
 
 bool HEARTRATE::pulseDetected() {
-    return wrist; //THIS IS TEST, IF WRIST DETECTED MAKE PULSE ANIMATIONS
+    return wrist_detected; //THIS IS TEST, IF WRIST DETECTED MAKE PULSE ANIMATIONS
 }
 
-void HEARTRATE::loop() {
-  auto sample = obj.readSample(1000);
-  float current_value = sample.red;
-  avgPulse = current_value;
-
-  if(sample.red > kFingerThreshold) {
-    if(millis() - finger_timestamp > kFingerCooldownMs) {
-      finger_detected = true;
-      wrist = true;
-      lastWrist = millis();
-    }
-  }
-
-  if(wrist) 
-  {
-    if((lastWrist + 1000) < millis())
-    {
-      wrist = false; 
-    }
-  }
-
+uint8_t HEARTRATE::getAvgPulse() {
+    // TODO: Maybe add functionality e.g. getAvgPulse(minute), getAvgPulse(hour) etc? Do the same for SpO2
+    return average_bpm;
 }
-
-int HEARTRATE::getAvgPulse() {
-    return avgPulse;
+uint8_t HEARTRATE::getAvgSpO2() {
+    return average_spo2;
 }
 
 bool HEARTRATE::wristDetected() { //käyttäjän ranne tunnistettu
-      return wrist;
+      return wrist_detected;
+}
+
+
+
+
+void HEARTRATE::loop() {
+  auto sample = obj.readSample(1000);
+  float current_value_red = sample.red;
+  float current_value_ir = sample.ir;
+  //avgPulse = current_value;
+
+  // Detect wrist using raw sensor value
+  if(sample.red > kDetectionThreshold) {
+    if(millis() - detection_timestamp > kDetectionCooldownMs) {
+      wrist_detected = true;
+    }
+  }
+  else {
+    // Reset values if the finger is removed
+    differentiator.reset();
+    averager_bpm.reset();
+    averager_r.reset();
+    averager_spo2.reset();
+    low_pass_filter_red.reset();
+    low_pass_filter_ir.reset();
+    high_pass_filter.reset();
+    stat_red.reset();
+    stat_ir.reset();
+    
+    wrist_detected = false;
+    detection_timestamp = millis();
+  }
+
+  if(wrist_detected) {
+    current_value_red = low_pass_filter_red.process(current_value_red);
+    current_value_ir = low_pass_filter_ir.process(current_value_ir);
+
+    // Statistics for pulse oximetry
+    stat_red.process(current_value_red);
+    stat_ir.process(current_value_ir);
+
+    // Heart beat detection using value for red LED
+    float current_value = high_pass_filter.process(current_value_red);
+    float current_diff = differentiator.process(current_value);
+
+    // Valid values?
+    if(!isnan(current_diff) && !isnan(last_diff)) {
+      
+      // Detect Heartbeat - Zero-Crossing
+      if(last_diff > 0 && current_diff < 0) {
+        crossed = true;
+        crossed_time = millis();
+      }
+      
+      if(current_diff > 0) {
+        crossed = false;
+      }
+  
+      // Detect Heartbeat - Falling Edge Threshold
+      if(crossed && current_diff < kEdgeThreshold) {
+        if(last_heartbeat != 0 && crossed_time - last_heartbeat > 300) {
+          // Show Results
+          int bpm = 60000/(crossed_time - last_heartbeat);
+          float rred = (stat_red.maximum()-stat_red.minimum())/stat_red.average();
+          float rir = (stat_ir.maximum()-stat_ir.minimum())/stat_ir.average();
+          float r = rred/rir;
+          float spo2 = kSpO2_A * r * r + kSpO2_B * r + kSpO2_C;
+          
+          if(bpm > 40 && bpm < 250) {
+            // Average?
+            // TODO: Implement averaging for different time periods, using default example averager for now
+            // TODO: Delete average_r if it's useless
+            // TODO: Reset averages if the finger is removed?
+            if(kEnableAveraging) {
+              average_bpm = averager_bpm.process(bpm);
+              average_r = averager_r.process(r);
+              average_spo2 = averager_spo2.process(spo2);
+  
+              // Show if enough samples have been collected
+              if(averager_bpm.count() >= kSampleThreshold) {
+                Serial.print("Time (ms): ");
+                Serial.println(millis()); 
+                Serial.print("Heart Rate (avg, bpm): ");
+                Serial.println(average_bpm);
+                Serial.print("R-Value (avg): ");
+                Serial.println(average_r);  
+                Serial.print("SpO2 (avg, %): ");
+                Serial.println(average_spo2);
+                Serial.print("Raw Red: ");
+                Serial.println(current_value_red);
+                Serial.println(wrist_detected);
+              }
+            }
+            else {
+              Serial.print("Time (ms): ");
+              Serial.println(millis()); 
+              Serial.print("Heart Rate (current, bpm): ");
+              Serial.println(bpm);  
+              Serial.print("R-Value (current): ");
+              Serial.println(r);
+              Serial.print("SpO2 (current, %): ");
+              Serial.println(spo2);   
+            }
+          }
+
+          // Reset statistic
+          stat_red.reset();
+          stat_ir.reset();
+        }
+  
+        crossed = false;
+        last_heartbeat = crossed_time;
+      }
+    }
+
+    last_diff = current_diff;
+  }
 }
--- Hardware/EspWatchObjectOriented/main/heartrate.h
+++ Hardware/EspWatchObjectOriented/main/heartrate.h
@@ -12,34 +12,86 @@ public:
     bool begin();
     bool pulseDetected();
     void loop();
-    int getAvgPulse();
+    uint8_t getAvgPulse();
+    uint8_t getAvgSpO2();
     bool wristDetected();
     void enableSensor();
     void disableSensor();
+
     bool sensorEnabled = false;
 
 private:
-    MAX30102 obj;
     bool wrist = false;
-    unsigned long lastWrist = 0;
 
-    const MAX30102::SamplingRate kSamplingRate = MAX30102::SAMPLING_RATE_3200SPS;
-    const float kSamplingFrequency = 400.0;
-    int avgPulse;
 
-    const int kAveragingSamples = 50;
+    // Sensor
+    MAX30102 obj;
+
+    // Sampling Rate
+    // Possible sampling rates: 50, 100, 200, 400, 800, 1000, 1600, 3200
+    const MAX30102::SamplingRate kSamplingRate = MAX30102::SAMPLING_RATE_400SPS;                        // TODO: Test different sampling rates to find the lowest possible rate for power saving with good accuracy
+    const float kSamplingFrequency = 400.0;                                                             // TODO: Investigate optimal sampling frequency, is it kSamplingRate = kSamplingFrequency?
+
+    // Wrist Detection threshold and cooldown
+    const unsigned long kDetectionThreshold = 10000;          // Change this to adjust sensitivity      // TODO: Implement DetectionThreshold calibration/auto-adjustment into the settings app
+    const unsigned int kDetectionCooldownMs = 500;
+    // Timestamp for wrist detection
+    long detection_timestamp = 0;
+    bool wrist_detected = false;
+
+    // Heartbeat detection
+    // Timestamp of the last heartbeat
+    long last_heartbeat = 0;
+    // Edge Detection Threshold
+    const float kEdgeThreshold = -2000.0;
+    // Last diff to detect zero crossing
+    float last_diff = NAN;
+    bool crossed = false;
+    long crossed_time = 0;
+
+
+    // Averaging
+    const bool kEnableAveraging = true;
+    static constexpr int kAveragingSamples = 5;
+    static constexpr int kSampleThreshold = 5;
+    uint8_t average_bpm;
+    uint8_t average_r;
+    uint8_t average_spo2;
+
 
     // Filters
-    const float kLowPassCutoff = 5.0;
-    const float kHighPassCutoff = 0.5;
+    static constexpr float kLowPassCutoff = 5.0;
+    static constexpr float kHighPassCutoff = 0.5;
+    // Filter Instances
+    LowPassFilter low_pass_filter_red;
+    LowPassFilter low_pass_filter_ir;
+    HighPassFilter high_pass_filter;
+    Differentiator differentiator;
+    MovingAverageFilter<kAveragingSamples> averager_bpm;
+    MovingAverageFilter<kAveragingSamples> averager_r;
+    MovingAverageFilter<kAveragingSamples> averager_spo2;
+    
     
 
-    // Finger Detection Threshold and Cooldown
-    const unsigned long kFingerThreshold = 10000;
-    const unsigned int kFingerCooldownMs = 500;
+    // Spo2
 
-    long finger_timestamp = 0;
-    bool finger_detected = false;
+    // Statistic for pulse oximetry
+    MinMaxAvgStatistic stat_red;
+    MinMaxAvgStatistic stat_ir;
+
+    // R value to SpO2 calibration factors
+    // See https://www.maximintegrated.com/en/design/technical-documents/app-notes/6/6845.html
+    float kSpO2_A = 1.5958422;
+    float kSpO2_B = -34.6596622;
+    float kSpO2_C = 112.6898759;
+
+
+
+
+    // TODO: Investigate the effects of different LED current values on power consumption and sensor accuracy, implement into the settings app (with calibration/auto-adjustment?)
+
+    // TODO: Investigate the effects of LED pulse width control on power consumption and sensor accuracy, implement into the settings app (with calibration/auto-adjustment?)
+    //  Resolution(bits) change with the change of pulse width, investigate the effects to accuracy
 };
 
 #endif
