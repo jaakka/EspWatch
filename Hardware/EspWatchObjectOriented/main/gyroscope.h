fb54119460d101082681ff41c972c6d2b2929b55
Paavo Perämäki <pate313@gmail.com>
2024-12-27 19:30:05 +0200

    Added functions from the gyroscope library, untested
    Compiles, needs to be tested
    
    More "custom" functions need to be added by using writeRegister() function of the library

--- Hardware/EspWatchObjectOriented/main/gyroscope.h
+++ Hardware/EspWatchObjectOriented/main/gyroscope.h
@@ -1,11 +1,35 @@
 #ifndef GYROSCOPE_H
 #define GYROSCOPE_H
 
+#include <Arduino_LSM6DS3.h>
+
 class GYROSCOPE {
 public:
+    GYROSCOPE();
+    bool begin();
+    void loop();
+
+
+    bool readAcceleration(float& x_Acc, float& y_Acc, float& z_Acc);
+    float getAccelerationSampleRate();
+    bool accelerationAvailable();
+
+    bool readGyroscope(float& x, float& y, float& z);
+    float getGyroscopeSampleRate();
+    bool gyroscopeAvailable();
+
+    bool readTemperature(float& t);
+    float getTemperatureSampleRate();
+    bool temperatureAvailable();
 
 private:
+
+    // Sensor
+    LSM6DS3Class obj;
 
 };
 
 #endif 
+
+// TODO: Implement calibration
+// TODO: Add functionality for setting sample rate, sleep mode etc. using writeRegister()
