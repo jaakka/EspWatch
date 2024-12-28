5210a4932eb9b298876b9dc1c2913c79df5f5549
Paavo Perämäki <pate313@gmail.com>
2024-12-28 02:07:46 +0200

    Implemented PCF8591 class

--- Hardware/EspWatchObjectOriented/main/pcf.h
+++ Hardware/EspWatchObjectOriented/main/pcf.h
@@ -1,10 +1,21 @@
 #ifndef PCF_H
 #define PCF_H
 
+#include <Adafruit_PCF8591.h>
+
 class PCF {
 public:
+    PCF();
+    bool begin();
+
+    uint8_t analogRead(uint8_t pin);
+    void analogWrite(uint8_t value);
+
+    void DAC_enabled(bool state);
 
 private:
+
+    Adafruit_PCF8591 obj;
 
 };
 
