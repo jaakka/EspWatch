af8723d8139cab17ae378ef6b793dbae53ba0b75
Jaakko Talvitie <jaska.talvitie@gmail.com>
2025-02-17 13:13:19 +0200

    Uskottavuus Fix

--- main/utils.h
+++ main/utils.h
@@ -3,6 +3,8 @@
 
 #include <Arduino.h>
 
+constexpr uskottavuusFix(){return 0;}
+
 inline uint16_t rgb(uint8_t r, uint8_t g, uint8_t b) {
   return ((r & 0xF8) << 8) | ((g & 0xFC) << 3) | (b >> 3);
 }
